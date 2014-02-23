02d061696d34fdf3d0f812773ecd4b05d2787fb0
RameshRM <rmrmail@gmail.com>
2014-02-23 08:25:24 -0800

    Create debouncer.js

---
+++ debouncer.js
@@ -0,0 +1,17 @@
+;(function($win){
+				"use strict";
+				
+				$win.debouncer = function(callback){
+					var __isFunction = typeof callback === "function";
+					
+					if (window.lastResizeInterval != null) {
+						window.clearInterval(window.lastResizeInterval);
+					}
+					
+					window.lastResizeInterval = window.setTimeout(function() {
+						if(__isFunction){
+							callback();
+						}
+					}, 200);
+				};
+			})(window);
